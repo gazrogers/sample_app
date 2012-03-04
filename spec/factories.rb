631e3ab6cf1e64d80cc2e58c3b13f65a926c0f81
Gareth Rogers <gareth@garethrogers.net>
2012-03-04 20:17:20 +0000

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,11 @@
 FactoryGirl.define do
 	factory :user do
-		name		"Gareth Rogers"
-		email		"gareth@garethrogers.net"
-		password	"foobar"
+		sequence(:name){|n|"Person #{n}"}
+		sequence(:email){|n|"person_#{n}@example.com"}
+		password "foobar"
+
+		factory :admin do
+			admin true
+		end
 	end
 end
